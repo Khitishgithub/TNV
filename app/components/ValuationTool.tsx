fdfb25bf8698d118c901d221f585bf86736cba34
Khitish Kumar Pradhan <126503968+Khitishgithub@users.noreply.github.com>
2025-01-29 17:52:38 +0530

    Update ValuationTool.tsx

--- app/components/ValuationTool.tsx
+++ app/components/ValuationTool.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 
 const ValuationTool = () => {
   return (
-    <section className="relative w-full">
+    <section className="relative w-full ">
       <div className="relative w-full">
-        {/* Container for the image */}
+       
         <div className="w-full relative">
           <img
-            className="w-full h-auto"
+            className="w-full min-h-[200px]"
             src="images/Valuation.png"
             alt="Valuation Tool"
           />
           
-          {/* Container for the button positioned over the image */}
+        
           <div className="absolute bottom-1/4 md:bottom-1/3 left-[9%]">
             <Link
               href="/valuation"
@@ -23,8 +23,8 @@ const ValuationTool = () => {
               Get Started
               <Plus 
                 className="text-yellow-300 ml-1" 
-                size={30}  
-                strokeWidth={5}
+                size={25}  
+                strokeWidth={4}
               />
             </Link>
           </div>
