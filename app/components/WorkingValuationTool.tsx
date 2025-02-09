4290e59c19ced08fe8395812f02f7a5077d36740
Khitish Kumar Pradhan <126503968+Khitishgithub@users.noreply.github.com>
2025-02-09 11:41:32 +0530

    Update WorkingValuationTool.tsx

--- app/components/WorkingValuationTool.tsx
+++ app/components/WorkingValuationTool.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WorkingValuationTool = () => {
   return (
-    <div className="min-h-[400px] w-full bg-black flex flex-col items-center justify-center px-6 font-['helveticanowtext-black-demo'] ">
+    <div className="w-full bg-black flex flex-col items-center justify-center px-6 font-['helveticanowtext-black-demo'] ">
       <div className="max-w-3xl space-y-8">
         <p className="text-white text-lg lg:text-2xl font-semibold leading-tight font-['helveticanowtext-black-demo'] ">
           Every business has a story, and understanding its value is a key part
